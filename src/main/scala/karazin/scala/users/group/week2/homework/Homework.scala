26fadeceb15925f45aebd16f4f0a54aece46bef5
Olena Linnyk <katyskarlett45@gmail.com>
2022-12-05 16:34:10 +0200

    Week-2 - <My first solutions for HW2>

--- src/main/scala/karazin/scala/users/group/week2/homework/Homework.scala
+++ src/main/scala/karazin/scala/users/group/week2/homework/Homework.scala
@@ -1,61 +1,65 @@
 package karazin.scala.users.group.week2.homework
-
 import scala.annotation.targetName
 import scala.math.{abs, signum}
-
 object Homework:
-
   // `x` and `y` are inaccessible from outside
   class Rational(x: Int, y: Int):
     // Checking the precondition. Is fails then throws `IllegalArgumentException`
     require(y > 0, "Denominator must be positive")
-
     def this(x: Int) = this(x, 1)
-
     val numer = x / g
     val denom = y / g
-
     // Defines an external name for a definition
     @targetName("less than")
     // Annotation on a method definition allows using the method as an infix operation
     infix def <(that: Rational): Boolean =
-      this.numer * that.denom < that.numer * this.denom
-
+      (this.numer * that.denom < that.numer * this.denom)
     @targetName("less or equal")
     infix def <=(that: Rational): Boolean =
-      this < that || this == that
-
+      (this < that || this == that)
     @targetName("greater than")
     infix def >(that: Rational): Boolean =
-      !(this <= that)
-
+      (!(this <= that))
     @targetName("greater or equal")
     infix def >=(that: Rational): Boolean =
-      !(this < that)
+      (!(this < that))
 
     @targetName("addition")
-    infix def +(that: Rational): Rational = ???
+    infix def +(that: Rational): Rational =
+      Rational(this.numer * that.denom + that.numer * this.denom, this.denom * that.denom)
 
     @targetName("negation")
-    infix def unary_- : Rational = ???
+    infix def unary_- : Rational =
+      Rational(0-this.numer, this.denom)
 
     @targetName("substraction")
-    infix def -(that: Rational): Rational = ???
+    infix def -(that: Rational): Rational =
+      Rational(this.numer * that.denom - that.numer * this.denom, this.denom * that.denom)
 
     @targetName("multiplication")
-    infix def *(that: Rational): Rational = ???
+    infix def *(that: Rational): Rational =
+      Rational(this.numer * that.numer, this.denom * that.denom)
 
     @targetName("devision")
-    infix def /(that: Rational): Rational = ???
+    infix def /(that: Rational): Rational = {
+      require(that.numer != 0, "Denominator is equal to zero")
+      val result_numer: Int = this.numer * that.denom
+      val result_denom: Int = this.denom * that.numer
+      Rational(signum(result_denom)*result_numer, abs(result_denom))
+    }
 
     override def toString: String = s"${this.numer}/${this.denom}"
 
     private def gcd(a: Int, b: Int): Int =
-      if b == 0 then a else gcd(b, a % b)
+      if b == 0
+      then a
+      else gcd(b, a % b)
 
     private lazy val g = gcd(abs(x), y)
-
-    override def equals(other: Any): Boolean = ???
+    override def equals(other: Any): Boolean = other match {
+      case that: Rational => ((this.numer == that.numer) && (this.denom == that.denom))
+      case _ => false
+    }
 
   end Rational
 
--- src/test/scala/karazin/scala/users/group/week2/homework/HomeworkSpecification.scala
+++ src/test/scala/karazin/scala/users/group/week2/homework/HomeworkSpecification.scala
@@ -1,13 +1,12 @@
 package karazin.scala.users.group.week2.homework
-
 import scala.math._
 import org.scalacheck._
 import Prop.{forAll, propBoolean, throws}
 import karazin.scala.users.group.week2.homework.arbitraries
 import Homework._
 import utils._
+object HomeworkSpecification extends Properties("Homework") :
 
-object HomeworkSpecification extends Properties("Homework"):
   import arbitraries.{given Arbitrary[Int], given Arbitrary[Rational]}
 
   property("throw exception due to zero denominator") = forAll { (numer: Int) ⇒
@@ -15,24 +14,19 @@ object HomeworkSpecification extends Properties("Homework"):
       Rational(numer, 0)
     }
   }
-
   property("throw exception due to negative denominator") = forAll { (numer: Int, kindaDenom: Int) ⇒
     throws(classOf[IllegalArgumentException]) {
       Rational(numer, -abs(kindaDenom))
     }
   }
-
   property("check that rational number is simplified") = forAll { (numer: Int, int: Int) ⇒
     val denom = abs(int) + 1
     val rational = Rational(numer, denom)
-
     rational.numer == (numer / gcd(abs(numer), denom)) && rational.denom == (denom / gcd(abs(numer), denom))
   }
-
   property("check equals") = forAll { (left: Rational, right: Rational) ⇒
     (left == right) == (left.numer == right.numer && left.denom == right.denom)
   }
-
   property("less then") = forAll { (left: Rational, right: Rational) =>
     (left < right) == (left.numer * right.denom < right.numer * left.denom)
   }
@@ -49,29 +43,31 @@ object HomeworkSpecification extends Properties("Homework"):
     (left >= right) == ( left > right || left == right)
   }
 
-  property("negation") = forAll { (rational: Rational) =>
-    ???
+  property("addition") = forAll { (left: Rational, right: Rational) =>
+    (Rational(left.numer * right.denom + right.numer * left.denom, left.denom * right.denom) == (left + right))
   }
 
-  property("addition") = forAll { (left: Rational, right: Rational) =>
-    ???
+  property("negation") = forAll { (rational: Rational) =>
+    (Rational((-1) * rational.numer, rational.denom) == (-rational))
   }
 
   property("subtraction") = forAll { (left: Rational, right: Rational) =>
-    ???
+    (Rational(left.numer * right.denom - right.numer * left.denom, left.denom * right.denom) == (left - right))
   }
 
   property("multiplication") = forAll { (left: Rational, right: Rational) =>
-    ???
+    (Rational(left.numer * right.numer, left.denom * right.denom) == (left * right))
   }
 
   property("division") = forAll { (left: Rational, numer: Int, denom: Int) =>
     val right = Rational(if numer == 0 then 1 else numer, abs(denom) + 1)
-    ???
+    Rational(signum(right.numer) * left.numer * right.denom, abs(left.denom * right.numer)) == (left / right)
   }
 
   property("division by zero") = forAll { (left: Rational, int: Int) =>
-    ???
+    throws(classOf[IllegalArgumentException]) {
+      left / Rational(int, 0)
+    }
   }
 
 end HomeworkSpecification
